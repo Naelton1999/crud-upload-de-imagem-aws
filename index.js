52bf837f6a565554aa12bbae67108a9ec947a8a3
Naelton1999 <Naelton1999>
2023-09-25 17:20:38 -0300

    main

--- index.js
+++ index.js
@@ -1,32 +1,100 @@
-const express= require('express');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+const { Readable } = require('stream');
 
 const server = express();
 server.use(express.json());
 
-const cursos=['Fullstack master', 'Desenvolvimento de games', 'Programação'];
+const s3 = new AWS.S3({
+  region: 'sa-east-1', 
+  credentials: {
+    accessKeyId: 'AKIAZOFDIGJ6WXTBJEXJ',
+    secretAccessKey: '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',
+  },
+});
 
-server.get('/cursos', (req,res)=>{
-    return res.json(cursos)
-})
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
-server.post('/cursos', (req,res)=>{
-    const uploadImageService = new UploadImageService();
-    const {name}= req.body
-    cursos.push(name)
-    return res.json(cursos)
-})
+server.post('/file', upload.single('imagem'), async (req, res) => {
+  try {
+    const { originalname } = req.file;
+    const stream = Readable.from(req.file.buffer);
+    const key = `${uuidv4()}${path.extname(originalname)}`;
 
-server.put('/cursos/:index', (req, res)=>{
-    const{index}= req.params
-    const{name}= req.body
-    cursos[index]= name
-    return res.json(cursos)
-})
+    const params = {
+      Bucket: 'crud-multer', 
+      Key: key,
+      Body: stream,
+    };
+    await s3.upload(params).promise();
 
-server.delete('/cursos/:index', (req, res)=>{
-    const {index}= req.params
-    cursos.slice(index,1)
-    return res.json({message:'o arquivo foi deletado com sucesso'})
-})
+    res.send('Upload feito com sucesso. URL: ' + `https://crud-multer.s3.sa-east-1.amazonaws.com/${key}`);
+  } catch (error) {
+    console.error('Erro ao fazer o upload para o S3:', error);
+    res.status(500).send('Erro ao fazer o upload para o S3');
+  }
+});
 
-server.listen(3332,()=> console.log('oi'))
+server.get('/images/:filename', (req, res) => {
+  try {
+    const { filename } = req.params;
+
+    const params = {
+      Bucket: 'crud-multer', 
+      Key: filename,
+    };
+
+    const signedUrl = s3.getSignedUrl('getObject', params);
+
+    res.redirect(signedUrl);
+  } catch (error) {
+    console.error('Erro ao obter a imagem do S3:', error);
+    res.status(500).send('Erro ao obter a imagem do S3');
+  }
+});
+
+server.put('/file/:filename', upload.single('imagem'), async (req, res) => {
+  try {
+    const { filename } = req.params;
+    const { originalname } = req.file;
+    const stream = Readable.from(req.file.buffer);
+
+    const params = {
+      Bucket: 'crud-multer',
+      Key: filename,
+      Body: stream,
+    };
+
+    await s3.putObject(params).promise();
+
+    res.send('Imagem atualizada com sucesso. URL: ' + `https://crud-multer.s3.sa-east-1.amazonaws.com/${filename}`);
+  } catch (error) {
+    console.error('Erro ao atualizar a imagem no S3:', error);
+    res.status(500).send('Erro ao atualizar a imagem no S3');
+  }
+});
+
+server.delete('/file/:filename', async (req, res) => {
+  try {
+    const { filename } = req.params;
+
+    const params = {
+      Bucket: 'crud-multer', 
+      Key: filename,
+    };
+
+    await s3.deleteObject(params).promise();
+
+    res.send('Imagem excluída com sucesso.');
+  } catch (error) {
+    console.error('Erro ao excluir a imagem no S3:', error);
+    res.status(500).send('Erro ao excluir a imagem no S3');
+  }
+});
+
+server.listen(3332, () => console.log('Servidor rodando na porta 3332'));
