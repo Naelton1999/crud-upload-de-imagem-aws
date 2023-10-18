c2e381ec9fcde56ad12e2008e6f85444843ff417
Naelton1999 <Naelton1999>
2023-10-18 09:58:50 -0300

    Adicionei novos recursos ao meu projeto

--- aws/aws.js
+++ aws/aws.js
@@ -3,8 +3,8 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
   region: 'sa-east-1',
   credentials: {
-    accessKeyId: 'AKIAZOFDIGJ6ZILJPF7W',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
   },
 });
 
